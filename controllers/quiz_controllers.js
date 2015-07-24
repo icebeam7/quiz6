81a117317dd611d27c9e2c6edbbd5e7bf1750861
Luis <luis.beltran@itcelaya.edu.mx>
2015-07-24 16:59:02 +0200

    Tarea (ultimo commit)
    Tarea (ultimo commit)

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -9,3 +9,13 @@ exports.answer = function (req, res){
   else
     res.render('quizes/answer', {respuesta: '¡Mala suerte! Tu respuesta ' + resp + ' es incorrecta =('});
 };
+
+exports.author = function (req, res){
+  res.render('author',
+    {
+      nombre: 'Luis Antonio Beltrán Prieto',
+      fotografia: 'https://pbs.twimg.com/profile_images/623445518388084736/B-5ErlHR_400x400.jpg',
+      twitter: 'https://twitter.com/darkicebeam',
+      video: 'http://www.youtube.com/embed/zkbwNAGh9J8'
+    });
+};
